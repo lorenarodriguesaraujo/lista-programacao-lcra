cbdd6fe8b13c3769b4f1a553b266b2c408142e4a
lorenaaraujo <lorodrigues049@gmail.com>
2025-08-20 21:20:12 -0300

    resumo da alteração

---
+++ AtividadeEmSala/src/AtividadeSala05.java
@@ -0,0 +1,12 @@
+public class AtividadeSala05 {
+    public static void main(String[] args){
+        char letra = 'A';
+        char inicial = 'V';
+        String texto = "Code";
+        String temp = "";
+        temp += "" + inicial + letra;
+        System.out.println(temp);
+        System.out.println(temp + texto);
+
+    }
+}
